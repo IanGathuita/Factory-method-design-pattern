6c10bc9aefbb0bed855ddb6e3708c70abaab349f
IanJoseph <Ianjoseph630@gmail.com>
2022-05-05 12:12:12 +0300

    Implementation of factory method design pattern using Typescript

---
+++ index.ts
@@ -0,0 +1,53 @@
+/*Unlike simple factory which  describes a class that has one creation method WITH A CONDITIONAL,
+Abstract factory provides an interface for creating objects but allows subclasses to alter the type
+of an object that will be created. */
+interface Animal{
+    printAnimalType():void;
+    printAnimalSound():void;
+}
+
+interface AnimalFactory{    
+    makeAnimal():Animal;
+}
+
+class Dog implements Animal{
+    printAnimalType() {
+        console.log("I am a dog");
+    }
+    printAnimalSound(): void {
+        console.log("Woof woof");
+    }
+}
+
+class Cat implements Animal{
+    printAnimalType() {
+        console.log("I am a cat");
+    }
+    printAnimalSound(): void {
+        console.log("Meowww");
+    }
+}
+
+
+class DogFactory implements AnimalFactory{    
+    makeAnimal() {
+        return new Dog();        
+    }
+}
+
+class CatFactory implements AnimalFactory{   
+    makeAnimal(){
+        return new Cat();
+    }
+}
+
+
+//client code
+let  myCat:Animal = new CatFactory().makeAnimal();
+let myDog:Animal = new DogFactory().makeAnimal();
+
+myCat.printAnimalSound();
+myCat.printAnimalType();
+
+myDog.printAnimalSound();
+myDog.printAnimalType();
